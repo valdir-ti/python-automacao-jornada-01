6fa287fec3e870e8be7eadb86ee915b40914a010
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-01-09 01:03:11 -0300

    feat: finish process

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pyautogui
 import subprocess
 import time
 import pyperclip
+import pandas
 
 # Pausa para dar tempo de focar no local correto
 time.sleep(1)
 
 # Abre o Google Chrome
-subprocess.run(['google-chrome'])
+subprocess.run(['google-chrome', '--incognito'])
 
 # Aguarda o Chrome abrir (ajuste conforme necessário)
 time.sleep(1)
@@ -25,15 +26,58 @@ pyautogui.hotkey('ctrl', 'v')
 # Pressiona Enter para abrir o link
 pyautogui.press('enter')
 
-time.sleep(1)
-pyautogui.click(x=1322, y=431)
+time.sleep(2)   
+# Pegando a posicao do campo de email
+pyautogui.click(x=1781, y=422)
 
+# Digitando o email no campo
 pyautogui.write('teste@gmail.com')
 
+# Indo para o próximo campo
 pyautogui.press('tab')
 
+# Digitando a senha no campo de senha
 pyautogui.write('12345')
 
+# Indo para o botão
 pyautogui.press('tab')
 
+# Apertando o botao para acessar o sistema
 pyautogui.press('enter')
+
+time.sleep(2)
+# importando a base de dados
+tabela = pandas.read_csv('produtos.csv')
+print(tabela)
+
+for linha in tabela.index:
+    pyautogui.click(x=1711, y=312)
+    # codigo
+    codigo = tabela.loc[linha, 'codigo']
+    pyautogui.write(codigo)
+    pyautogui.press('tab')
+    # marca
+    marca = tabela.loc[linha, 'marca']
+    pyautogui.write(marca)
+    pyautogui.press('tab')
+    # tipo
+    tipo = tabela.loc[linha, 'tipo']
+    pyautogui.write(tipo)
+    pyautogui.press('tab')
+    # categoria
+    categoria = str(tabela.loc[linha, 'categoria'])
+    pyautogui.write(categoria)
+    pyautogui.press('tab')
+    # preco_unitario
+    preco_unitario = str(tabela.loc[linha, 'preco_unitario'])
+    pyautogui.write(preco_unitario)
+    pyautogui.press('tab')
+    # custo
+    custo = str(tabela.loc[linha, 'custo'])
+    pyautogui.write(custo)
+    pyautogui.press('tab')
+    # obs
+    obs = tabela.loc[linha, 'obs']
+    pyautogui.write(str(obs) if not pandas.isna(obs) else '')
+    pyautogui.press('tab')
+    pyautogui.press('enter')
